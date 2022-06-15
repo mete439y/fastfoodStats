b9563e674a4a31d495c804e060ef9926e412775b
Tenaw Derseh <mete439@gmail.com>
2022-06-15 14:28:50 -0700

    q4 fixed

--- fastfoodStats.R
+++ fastfoodStats.R
@@ -7,7 +7,7 @@ suppressPackageStartupMessages(library(openintro))
 Q1<-cor(fastfood[fastfood$restaurant %in% c("Sonic", "Subway", "Taco Bell"),
                  c('calories', 'total_fat', 'sugar', 'calcium')], 
         use="pairwise.complete.obs", method="pearson")
-Q1
+
 
 Q02 <- openintro::fastfood %>% 
   filter(restaurant %in% c("Mcdonalds", "Subway")) %>%
@@ -17,14 +17,16 @@ fit_Q02 <- glm(is_mcdonalds ~ calories + sodium + protein,
 summary(fit_Q02)
 
 Q2 <- round(coef(fit_Q02), 2)
-Q2
+
 
 fit_Q03 <- glm(is_mcdonalds ~ calories + protein, 
                family = "binomial", data = fastfood)
 summary(fit_Q03)
 
 Q3 <- fit_Q03$aic
-Q3
+
+Q4 <- lm(calories ~ fiber+ sugar, fastfood)
+
 
 Q05 <- openintro::fastfood %>%
   group_by(restaurant) %>%
@@ -38,4 +40,4 @@ fit_Q05 <- update(fit_Q05, . ~ . - vit_a, - restaurant)
 coefs_Q05 <- round(coef(lm.beta::lm.beta(fit_Q05)), 2)
 
 Q5 <- coefs_Q05[which.max(coefs_Q05)]
-Q5
+
